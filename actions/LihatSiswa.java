ea5a4d62b9af06d37609de58109df28638ac4a7d
ArRaudhatulPutriMuhida <putri.muhida@gmail.com>
2025-11-24 13:28:08 +0700

    Add LihatSiswa class to display student data

--- actions/LihatSiswa.java
+++ actions/LihatSiswa.java
@@ -1,1 +1,21 @@
+package actions;
 
+import main.Aplikasi;
+import model.Siswa;
+import model.User;
+
+public class LihatSiswa {
+    public void eksekusi() {
+        Aplikasi.cls();
+        System.out.println("--- DATA SISWA ---");
+        int no = 1;
+        for (User u : Aplikasi.userList) {
+            if (u instanceof Siswa) {
+                System.out.println(no + ". " + u.toString());
+                no++;
+            }
+        }
+        if (no == 1) System.out.println("Belum ada data.");
+        // Kita tidak pakai pressEnter disini agar bisa dipakai oleh HapusSiswa tanpa pause
+    }
+}
